b8160550f604fa5b9e7f8593511de32a7a2ae114
laharikabattulla <laharikabattulla@gmail.com>
2026-09-21 15:30:58 +0530

    Add files via upload

---
+++ class with multiple methods.py
@@ -0,0 +1,14 @@
+class Demo():
+    def addition(self,a,b):
+        print("Addition:",a+b)
+    def subtraction(self,a,b):
+        print("Subtraction:",a-b)
+    def multiply(self,a,b):
+        print("Multiply:",a*b)
+    def divide(self,a,b):
+        print("Divide:",a/b)
+d1=Demo()
+d1.addition(17,26)
+d1.subtraction(26,17)
+d1.multiply(5,7)
+d1.divide(28,4)
