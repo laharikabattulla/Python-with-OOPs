5d89cc26545822975b86e28277b14b3a86e32988
laharikabattulla <laharikabattulla@gmail.com>
2026-09-21 15:04:24 +0530

    Add files via upload

---
+++ creation of class and object.py
@@ -0,0 +1,14 @@
+class student:
+    name="Laharika"
+    age=18
+    def read(self):
+        print("reading")
+    def write(self):
+        print("wrinting")
+s1=student()
+print(s1.name)
+print(s1.age)
+s1.read()
+s1.write()
+    
+    
